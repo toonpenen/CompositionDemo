78ba7419e8724e437e84b80db26f4034dcbb237f
toonpenen <Toon.Penen@student.intecbrussel.be>
2020-11-30 10:00:37 +0100

    Add project files.

---
+++ CompositionDemo/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositionDemo.Models
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string School { get; set; }
+        public string Adress { get; set; }
+    }
+}
